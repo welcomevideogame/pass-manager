e585afdd40f4b02621240e087547c6f85615f0ce
welcomevideogame <99519510+welcomevideogame@users.noreply.github.com>
2022-12-14 19:02:01 -0500

    Security Improvements and Optimizations
    Moved to better randomness libraries

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import gui
 
 if __name__ == "__main__":
-    screen = gui.PassGUI()
+    gui.PassGUI()
--- passGen.py
+++ passGen.py
@@ -1,44 +1,35 @@
 import string
-import random
 import requests
 import secrets
 
 class Generator:
+    csprng = secrets.SystemRandom()
 
-    @staticmethod
-    def generate_password(config, length=16):
-        chars = ''.join([
-            string.digits if config[1] else '',
-            string.ascii_lowercase if config[2] else '',
-            string.ascii_uppercase if config[3] else '',
-            '!@#$%^&*' if config[4] else '', ])
-        password = ''.join([secrets.choice(chars) for _ in range(length)])
-        print(password)
-        return password.encode('utf-8')
+    @classmethod
+    def generate_password(cls, config, length=16):
+        char_list = cls.get_chars(config)
+        rand_bytes = secrets.token_bytes(length)
+        password = [char_list[byte % len(char_list)] for byte in rand_bytes]
+        cls.csprng.shuffle(password)
+        return "".join(password)
 
-
-    @staticmethod
-    def generate_true_password(config, length=16):
-        chars = ''
-        chars2 = ''
-        char_list = ["!@#$%^&*", string.digits, string.ascii_lowercase, string.ascii_uppercase, ",.';-"]
-        for i in range(len(config)):
-            if config[i]:
-                try:
-                    source = f"https://www.random.org/integers/?num={length}&min=0&max={len(char_list[i])}&col=5&base=10&format=plain&rnd=new"
-                    nums = requests.get(source).text.replace("\n", "\t").split("\t")[:-1]
-                    random.SystemRandom().shuffle(nums)
-                    for j in nums:
-                        chars += char_list[i][int(j) - 1]
-                except requests.exceptions.ConnectionError:
-                    return None
+    @classmethod
+    def generate_true_password(cls, config, length=16):
+        char_list = cls.get_chars(config)
         try:
-            source = f"https://www.random.org/integers/?num={length}&min=0&max={len(chars)}&col=5&base=10&format=plain&rnd=new"
+            source = f"https://www.random.org/integers/?num={length}&min=0&max={len(char_list) - 1}&col=5&base=10&format=plain&rnd=new"
             nums = requests.get(source).text.replace("\n", "\t").split("\t")[:-1]
-            random.SystemRandom().shuffle(nums)
-            for i in nums:
-                chars2 += chars[int(i) - 1]
         except requests.exceptions.ConnectionError:
             return None
-        return chars2
+        password = [char_list[int(num)] for num in nums]
+        cls.csprng.shuffle(password)
+        return "".join(password)
+
+    @staticmethod
+    def get_chars(config):
+        return ''.join([
+        string.digits if config[1] else '',
+        string.ascii_lowercase if config[2] else '',
+        string.ascii_uppercase if config[3] else '',
+        '!@#$%^&*' if config[4] else '', ])
 
